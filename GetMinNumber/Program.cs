9cf972373957984782c52542ab1ddef2b5f62e92
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-22 12:26:30 +0530

    Add project files.

---
+++ GetMinNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace GetMinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 15, 18, -8, -9, -7, 20 };
+            int min = numbers.Min();
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Minimum value in array : " + min);
+        }
+    }
+}
